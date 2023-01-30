fea3039a412898f31192b6f17ef80d2841097530
koolunkle <a43361904@gmail.com>
2023-01-30 22:44:52 +0900

    UI 세팅

--- app/src/main/java/com/udemy/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/udemy/a7minutesworkout/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.udemy.a7minutesworkout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flStartButton: FrameLayout = findViewById(R.id.fl_start)
+        flStartButton.setOnClickListener {
+            Toast.makeText(this, "Here we will start the exercise", Toast.LENGTH_SHORT).show()
+        }
     }
 }
