a75ddcbca0bde2e1c2ea40976f28e16674466da0
DESKTOP-IS0KJUV\mr05-04 <a43361904@gmail.com>
2023-01-31 11:12:31 +0900

    CountDownTimer 통해 잔여 초 표시

--- app/src/main/java/com/udemy/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/udemy/a7minutesworkout/ExerciseActivity.kt
@@ -1,6 +1,8 @@
 package com.udemy.a7minutesworkout
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.udemy.a7minutesworkout.databinding.ActivityExerciseBinding
 
@@ -9,6 +11,10 @@ class ExerciseActivity : AppCompatActivity() {
     private var _binding: ActivityExerciseBinding? = null
 
     private val binding get() = _binding!!
+
+    private var restTimer: CountDownTimer? = null
+
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +28,43 @@ class ExerciseActivity : AppCompatActivity() {
         binding.toolbarExercise.setNavigationOnClickListener {
             onBackPressed()
         }
+        setupRestView()
     }
 
     override fun onDestroy() {
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
         _binding = null
         super.onDestroy()
+    }
+
+    private fun setupRestView() {
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar() {
+        binding.progressBar.progress = restProgress
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(p0: Long) {
+                restProgress++
+                binding.progressBar.progress = 10 - restProgress
+                binding.tvTimer.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    applicationContext,
+                    "Here now we will start the exercise",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }.start()
     }
 
 }
