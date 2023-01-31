bf828f562f7a1d7bbf5e86be87d31d5b35e6a7b3
koolunkle <a43361904@gmail.com>
2023-01-31 23:39:10 +0900

    미디어 플레이어 추가

--- app/src/main/java/com/udemy/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/udemy/a7minutesworkout/ExerciseActivity.kt
@@ -1,5 +1,7 @@
 package com.udemy.a7minutesworkout
 
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.speech.tts.TextToSpeech
@@ -29,6 +31,8 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private var currentExercisePosition = -1
 
     private var tts: TextToSpeech? = null
+
+    private var player: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,11 +66,22 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             tts!!.stop()
             tts!!.shutdown()
         }
+        if (player != null) player!!.stop()
         _binding = null
         super.onDestroy()
     }
 
     private fun setupRestView() {
+        try {
+            val soundURI =
+                Uri.parse("android.resource://com.udemy.a7minutesworkout/" + R.raw.press_start)
+            player = MediaPlayer.create(applicationContext, soundURI)
+            player?.isLooping = false
+            player?.start()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
         binding.flRestView.visibility = View.VISIBLE
         binding.tvTitle.visibility = View.VISIBLE
         binding.tvExerciseName.visibility = View.INVISIBLE
