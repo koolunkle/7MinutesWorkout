7dc066c2d2144519e20ab170d43f6e7edfab00e4
koolunkle <a43361904@gmail.com>
2023-02-02 22:22:00 +0900

    룸 데이터베이스 추가

--- app/src/main/java/com/udemy/a7minutesworkout/FinishActivity.kt
+++ app/src/main/java/com/udemy/a7minutesworkout/FinishActivity.kt
@@ -1,8 +1,13 @@
 package com.udemy.a7minutesworkout
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.udemy.a7minutesworkout.databinding.ActivityFinishBinding
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
 
@@ -25,6 +30,24 @@ class FinishActivity : AppCompatActivity() {
 
         binding.btnFinish.setOnClickListener {
             finish()
+        }
+
+        val historyDao = (application as WorkoutApp).database.historyDao()
+        addDateToDatabase(historyDao)
+    }
+
+    private fun addDateToDatabase(historyDao: HistoryDao) {
+        val calendar = Calendar.getInstance()
+        val dateTime = calendar.time
+        Log.e("Date: ", "" + dateTime)
+
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss", Locale.getDefault())
+        val date = sdf.format(dateTime)
+        Log.e("Formatted Date : ", "" + date)
+
+        lifecycleScope.launch {
+            historyDao.insert(HistoryEntity(date))
+            Log.e("Date : ", "Added...")
         }
     }
 
