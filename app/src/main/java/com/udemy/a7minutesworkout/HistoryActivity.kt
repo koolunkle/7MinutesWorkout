ffd33dfa35098752253c33bfe0534fb86257c4f4
koolunkle <a43361904@gmail.com>
2023-02-02 23:08:08 +0900

    리사이클러뷰 통해 운동 히스토리 표시

--- app/src/main/java/com/udemy/a7minutesworkout/HistoryActivity.kt
+++ app/src/main/java/com/udemy/a7minutesworkout/HistoryActivity.kt
@@ -1,8 +1,12 @@
 package com.udemy.a7minutesworkout
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.udemy.a7minutesworkout.databinding.ActivityHistoryBinding
+import kotlinx.coroutines.launch
 
 class HistoryActivity : AppCompatActivity() {
 
@@ -23,6 +27,33 @@ class HistoryActivity : AppCompatActivity() {
 
         binding.toolbarHistoryActivity.setNavigationOnClickListener {
             onBackPressed()
+        }
+
+        val historyDao = (application as WorkoutApp).database.historyDao()
+        getAllCompletedDates(historyDao)
+    }
+
+    private fun getAllCompletedDates(historyDao: HistoryDao) {
+        lifecycleScope.launch {
+            historyDao.fetchAllDates().collect { allCompletedDatesList ->
+                if (allCompletedDatesList.isNotEmpty()) {
+                    binding.tvHistory.visibility = View.VISIBLE
+                    binding.rvHistory.visibility = View.VISIBLE
+                    binding.tvNoDataAvailable.visibility = View.INVISIBLE
+                    binding.rvHistory.layoutManager = LinearLayoutManager(applicationContext)
+
+                    val dates = ArrayList<String>()
+                    for (date in allCompletedDatesList) {
+                        dates.add(date.date)
+                    }
+                    val historyAdapter = HistoryAdapter(dates)
+                    binding.rvHistory.adapter = historyAdapter
+                } else {
+                    binding.tvHistory.visibility = View.GONE
+                    binding.rvHistory.visibility = View.GONE
+                    binding.tvNoDataAvailable.visibility = View.VISIBLE
+                }
+            }
         }
     }
 
